9c89e00c8198f92b35cabde6819832512d829480
vigneshwarsridharan <viky.viky884@gmail.com>
2023-05-06 11:22:10 +0000

    Update 8 files

--- website/app/page.js
+++ website/app/page.js
@@ -1,53 +1,253 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+import APIService from "../lib/APIService";
 
-export default function Home() {
+const getData = async () => {
+  const res = await APIService.get("/todos/1");
+  console.log("getData", res.data);
+  return res.data;
+};
+
+export default async function Home() {
+  const res = await getData();
+  console.log("Home", res);
   return (
     <>
-      {/* <!-- ======= Header ======= --> */}
-      <header id="header" className="header d-flex align-items-center">
-        <div className="container-fluid container-xl d-flex align-items-center justify-content-between">
+      {/* <!-- ======= Hero Section ======= --> */}
+      <section id="hero" className="hero">
+        <div className="info d-flex align-items-center">
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-lg-6 text-center">
+                <h2>
+                  Welcome to <span>Chennai Property Service</span>
+                </h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+                <a href="#get-started" className="btn-get-started">
+                  Get Started
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
 
-          <a href="index.html" className="logo d-flex align-items-center">
-            {/* <!-- Uncomment the line below if you also wish to use an image logo --> */}
-            {/* <!-- <img src="assets/img/logo.png" alt=""> --> */}
-            <h1>UpConstruction<span>.</span></h1>
+        <div
+          id="hero-carousel"
+          className="carousel slide"
+          data-bs-ride="carousel"
+          data-bs-interval="5000"
+        >
+          <div
+            className="carousel-item active"
+            style={{
+              backgroundImage:
+                "url(assets/img/hero-carousel/hero-carousel-1.jpg)",
+            }}
+          ></div>
+          <div
+            className="carousel-item"
+            style={{
+              backgroundImage:
+                "url(assets/img/hero-carousel/hero-carousel-2.jpg)",
+            }}
+          ></div>
+          <div
+            className="carousel-item"
+            style={{
+              backgroundImage:
+                "url(assets/img/hero-carousel/hero-carousel-3.jpg)",
+            }}
+          ></div>
+          <div
+            className="carousel-item"
+            style={{
+              backgroundImage:
+                "url(assets/img/hero-carousel/hero-carousel-4.jpg)",
+            }}
+          ></div>
+          <div
+            className="carousel-item"
+            style={{
+              backgroundImage:
+                "url(assets/img/hero-carousel/hero-carousel-5.jpg)",
+            }}
+          ></div>
+
+          <a
+            className="carousel-control-prev"
+            href="#hero-carousel"
+            role="button"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon bi bi-chevron-left"
+              aria-hidden="true"
+            ></span>
           </a>
 
-          <i className="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
-          <i className="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
-          <nav id="navbar" className="navbar">
-            <ul>
-              <li><a href="index.html" className="active">Home</a></li>
-              <li><a href="about.html">About</a></li>
-              <li><a href="services.html">Services</a></li>
-              <li><a href="projects.html">Projects</a></li>
-              <li><a href="blog.html">Blog</a></li>
-              <li className="dropdown"><a href="#"><span>Dropdown</span> <i className="bi bi-chevron-down dropdown-indicator"></i></a>
-                <ul>
-                  <li><a href="#">Dropdown 1</a></li>
-                  <li className="dropdown"><a href="#"><span>Deep Dropdown</span> <i className="bi bi-chevron-down dropdown-indicator"></i></a>
-                    <ul>
-                      <li><a href="#">Deep Dropdown 1</a></li>
-                      <li><a href="#">Deep Dropdown 2</a></li>
-                      <li><a href="#">Deep Dropdown 3</a></li>
-                      <li><a href="#">Deep Dropdown 4</a></li>
-                      <li><a href="#">Deep Dropdown 5</a></li>
-                    </ul>
-                  </li>
-                  <li><a href="#">Dropdown 2</a></li>
-                  <li><a href="#">Dropdown 3</a></li>
-                  <li><a href="#">Dropdown 4</a></li>
-                </ul>
-              </li>
-              <li><a href="contact.html">Contact</a></li>
-            </ul>
-          </nav>
-          {/* <!-- .navbar --> */}
-
+          <a
+            className="carousel-control-next"
+            href="#hero-carousel"
+            role="button"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon bi bi-chevron-right"
+              aria-hidden="true"
+            ></span>
+          </a>
         </div>
-      </header>
-      {/* <!-- End Header --> */}
+      </section>
+      {/* <!-- End Hero Section --> */}
+
+      <main id="main">
+        {/* <!-- ======= Recent Blog Posts Section ======= --> */}
+        <section id="recent-blog-posts" className="recent-blog-posts">
+          <div className="container">
+            <div className=" section-header">
+              <h2>Recent Blog Posts</h2>
+              <p>
+                In commodi voluptatem excepturi quaerat nihil error autem
+                voluptate ut et officia consequuntu
+              </p>
+            </div>
+
+            <div className="row gy-5">
+              <div className="col-xl-4 col-md-6">
+                <div className="post-item position-relative h-100">
+                  <div className="post-img position-relative overflow-hidden">
+                    <img
+                      src="assets/img/blog/blog-1.jpg"
+                      className="img-fluid"
+                      alt=""
+                    />
+                    <span className="post-date">December 12</span>
+                  </div>
+
+                  <div className="post-content d-flex flex-column">
+                    <h3 className="post-title">
+                      Eum ad dolor et. Autem aut fugiat debitis
+                    </h3>
+
+                    <div className="meta d-flex align-items-center">
+                      <div className="d-flex align-items-center">
+                        <i className="bi bi-person"></i>{" "}
+                        <span className="ps-2">Julia Parker</span>
+                      </div>
+                      <span className="px-3 text-black-50">/</span>
+                      <div className="d-flex align-items-center">
+                        <i className="bi bi-folder2"></i>{" "}
+                        <span className="ps-2">Politics</span>
+                      </div>
+                    </div>
+
+                    <hr />
+
+                    <a
+                      href="blog-details.html"
+                      className="readmore stretched-link"
+                    >
+                      <span>Read More</span>
+                      <i className="bi bi-arrow-right"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              {/* <!-- End post item --> */}
+
+              <div className="col-xl-4 col-md-6">
+                <div className="post-item position-relative h-100">
+                  <div className="post-img position-relative overflow-hidden">
+                    <img
+                      src="assets/img/blog/blog-2.jpg"
+                      className="img-fluid"
+                      alt=""
+                    />
+                    <span className="post-date">July 17</span>
+                  </div>
+
+                  <div className="post-content d-flex flex-column">
+                    <h3 className="post-title">
+                      Et repellendus molestiae qui est sed omnis
+                    </h3>
+
+                    <div className="meta d-flex align-items-center">
+                      <div className="d-flex align-items-center">
+                        <i className="bi bi-person"></i>{" "}
+                        <span className="ps-2">Mario Douglas</span>
+                      </div>
+                      <span className="px-3 text-black-50">/</span>
+                      <div className="d-flex align-items-center">
+                        <i className="bi bi-folder2"></i>{" "}
+                        <span className="ps-2">Sports</span>
+                      </div>
+                    </div>
+
+                    <hr />
+
+                    <a
+                      href="blog-details.html"
+                      className="readmore stretched-link"
+                    >
+                      <span>Read More</span>
+                      <i className="bi bi-arrow-right"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              {/* <!-- End post item --> */}
+
+              <div className="col-xl-4 col-md-6">
+                <div className="post-item position-relative h-100">
+                  <div className="post-img position-relative overflow-hidden">
+                    <img
+                      src="assets/img/blog/blog-3.jpg"
+                      className="img-fluid"
+                      alt=""
+                    />
+                    <span className="post-date">September 05</span>
+                  </div>
+
+                  <div className="post-content d-flex flex-column">
+                    <h3 className="post-title">
+                      Quia assumenda est et veritati tirana ploder
+                    </h3>
+
+                    <div className="meta d-flex align-items-center">
+                      <div className="d-flex align-items-center">
+                        <i className="bi bi-person"></i>{" "}
+                        <span className="ps-2">Lisa Hunter</span>
+                      </div>
+                      <span className="px-3 text-black-50">/</span>
+                      <div className="d-flex align-items-center">
+                        <i className="bi bi-folder2"></i>{" "}
+                        <span className="ps-2">Economics</span>
+                      </div>
+                    </div>
+
+                    <hr />
+
+                    <a
+                      href="blog-details.html"
+                      className="readmore stretched-link"
+                    >
+                      <span>Read More</span>
+                      <i className="bi bi-arrow-right"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              {/* <!-- End post item --> */}
+            </div>
+          </div>
+        </section>
+        {/* <!-- End Recent Blog Posts Section --> */}
+      </main>
     </>
-  )
+  );
 }
